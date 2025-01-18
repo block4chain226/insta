eb782e6c619f82567bf1d34d4e08ab8a037739ff
Ivan Ivanov <backtobackend@proton.me>
2025-01-18 15:24:47 -0800

    UserModelFactory save new user to databaselogic

--- apps/users-app/src/adapters/user.repository.ts
+++ apps/users-app/src/adapters/user.repository.ts
@@ -2,8 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { BaseRepository } from 'libs/common/abstract/repository.abstract';
 import { IRepositoryAbstract } from 'libs/common/ports/out/repository-abstract.interface';
 import { User } from '../model/User.model';
+import { ResponseUserDto } from 'libs/User/dto/response-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserRepository
   extends BaseRepository<User, ResponseUserDto>
-  implements IRepositoryAbstract<User, ResponseUserDto> {}
+  implements IRepositoryAbstract<User, ResponseUserDto>
+{
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {
+    super(userRepository);
+  }
+}
--- apps/users-app/src/factory/user-model.factory.ts
+++ apps/users-app/src/factory/user-model.factory.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { IModelFactory } from 'libs/common/interfaces/model-factory.interface';
 import { User } from '../model/User.model';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserRepository } from '../adapters/user.repository';
 
 @Injectable()
 export class UserModelFactory implements IModelFactory<User> {
+  constructor(private readonly userRepository: UserRepository) {}
   create(name: string, email: string, password: string): User {
     const user = new User(name, email, password);
+    const newUser = this.userRepository.create(user);
   }
 }
