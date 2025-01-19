b766f051fe939fcf310e732cc3977e4df04e7610
Ivan Ivanov <backtobackend@proton.me>
2025-01-18 20:18:43 -0800

    users gateway, env postgres

--- apps/insta-gateway/src/insta-gateway.module.ts
+++ apps/insta-gateway/src/insta-gateway.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InstaGatewayController } from './insta-gateway.controller';
 import { InstaGatewayService } from './insta-gateway.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [InstaGatewayController],
   providers: [InstaGatewayService],
 })
