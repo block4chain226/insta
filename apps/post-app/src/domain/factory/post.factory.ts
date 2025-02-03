19b68b5dbdb8c8a5347b851e27edfccc4918c46a
Ivan Ivanov <backtobackend@proton.me>
2025-02-02 22:12:12 -0800

    success

--- apps/post-app/src/domain/factory/post.factory.ts
+++ apps/post-app/src/domain/factory/post.factory.ts
@@ -3,6 +3,7 @@ import { IModelFactory } from 'libs/common/interfaces/model-factory.interface';
 import { Post } from '../models/Post.model';
 import { ResponsePostDto } from 'libs/Post/dto/response-post.dto';
 import { CreatePostDto } from 'libs/Post/dto/create-post.dto';
+import { Content } from '../models/Content.model';
 
 @Injectable()
 export class PostFactory implements IModelFactory<Post, ResponsePostDto> {
@@ -12,6 +13,8 @@ export class PostFactory implements IModelFactory<Post, ResponsePostDto> {
     createPostDto: CreatePostDto,
     files: Express.Multer.File[],
   ): Promise<ResponsePostDto> {
+    const content = Content.create('image/jpeg', 'http//:vasa.com/111', '111');
+    console.log('🚀 ~ PostFactory ~ content:', content);
     return;
   }
 }
